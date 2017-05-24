e79e2e3f8ab218e6d53684ae28f46bce6ed9dd3d
Antoine Rennuit <are@lescompanions.com>
2017-05-24 19:27:04 +0200

    [MocapFusion] Touchy variables renaming (+ introduced temporaries) to make code reading easier.

--- InfiniTAM/ITMLib/Engine/DeviceAgnostic/ITMDepthTracker.h
+++ InfiniTAM/ITMLib/Engine/DeviceAgnostic/ITMDepthTracker.h
@@ -19,83 +19,95 @@ _CPU_AND_GPU_CODE_ inline bool computePerPointGH_Depth_Ab(THREADPTR(float) *A, T
 
     // Compute s (expressed in frame k^z)
     // NOTE: inverse pinhole model (2D -> 3D).
-    Vector4f tmp3Dpoint;
+    Vector4f s_kz;
 
     float fx = viewIntrinsics.x;
     float fy = viewIntrinsics.y;
     float cx = viewIntrinsics.z;
     float cy = viewIntrinsics.w;
 
-    tmp3Dpoint.x = depth * ((float(x) - cx) / fx);
-    tmp3Dpoint.y = depth * ((float(y) - cy) / fy);
-	tmp3Dpoint.z = depth;
-	tmp3Dpoint.w = 1.0f;
+    s_kz.x = depth * ((float(x) - cx) / fx);
+    s_kz.y = depth * ((float(y) - cy) / fy);
+    s_kz.z = depth;
+    s_kz.w = 1.0f;
     
     // Express s in world frame.
+    // NOTE: as we do not know frame k^z we approximate it by frame {k^(z-1)}.
+    //       And the whole point for the optimization below is to make up for
+    //       this approximation.
     // NOTE: approxInvPose = H_{k^(z-1)}_0.
-	tmp3Dpoint = approxInvPose * tmp3Dpoint;
-    tmp3Dpoint.w = 1.0f; // Enforce w = 1.
+    Vector4f s_0;
+
+    s_0 = approxInvPose * s_kz;
+    s_0.w = 1.0f; // Enforce w = 1.
 
     // Express s in frame k-1.
     // NOTE: scenePose = H_0_{k-1}.
-    Vector4f tmp3Dpoint_reproj;
+    Vector4f s_km1;
 
-    tmp3Dpoint_reproj = scenePose * tmp3Dpoint;
-    if (tmp3Dpoint_reproj.z <= 0.0f)
+    s_km1 = scenePose * s_0;
+
+    if (s_km1.z <= 0.0f)
         return false;
 
     // Compute corresponding pixel in k-1.
     // NOTE: pinhole model.
-    Vector2f tmp2Dpoint;
+    Vector2f d_2d;
 
-    tmp2Dpoint.x = sceneIntrinsics.x * tmp3Dpoint_reproj.x / tmp3Dpoint_reproj.z + sceneIntrinsics.z;
-	tmp2Dpoint.y = sceneIntrinsics.y * tmp3Dpoint_reproj.y / tmp3Dpoint_reproj.z + sceneIntrinsics.w;
+    d_2d.x = sceneIntrinsics.x * s_km1.x / s_km1.z + sceneIntrinsics.z;
+    d_2d.y = sceneIntrinsics.y * s_km1.y / s_km1.z + sceneIntrinsics.w;
 
-	if (!((tmp2Dpoint.x >= 0.0f) && (tmp2Dpoint.x <= sceneImageSize.x - 2) && (tmp2Dpoint.y >= 0.0f) && (tmp2Dpoint.y <= sceneImageSize.y - 2)))
+    if (!((d_2d.x >= 0.0f) && (d_2d.x <= sceneImageSize.x - 2) && (d_2d.y >= 0.0f) && (d_2d.y <= sceneImageSize.y - 2)))
 		return false;
 
     // Compute corresponding destination point d.
-    // NOTE: it is expressed in frame k-1.
-    Vector4f curr3Dpoint;
-    Vector4f corr3Dnormal;
+    // NOTE: it is expressed in frame 0.
+    Vector4f d_0;
 
-	curr3Dpoint = interpolateBilinear_withHoles(pointsMap, tmp2Dpoint, sceneImageSize);
-    if (curr3Dpoint.w < 0.0f)
+    d_0 = interpolateBilinear_withHoles(pointsMap, d_2d, sceneImageSize);
+
+    if (d_0.w < 0.0f)
         return false;
+
+    // Compute normal at d (expressed in frame 0).
+    Vector4f n_0;
+    n_0 = interpolateBilinear_withHoles(normalsMap, d_2d, sceneImageSize);
+
+//    if (n_0.w < 0.0f)
+//        return false;
 
     // Optimization: compute contribution to A and b.
     // NOTE: the optimization aligns s on d (via movement M).
     Vector3f ptDiff;
 
-	ptDiff.x = curr3Dpoint.x - tmp3Dpoint.x;
-	ptDiff.y = curr3Dpoint.y - tmp3Dpoint.y;
-	ptDiff.z = curr3Dpoint.z - tmp3Dpoint.z;
+    ptDiff.x = d_0.x - s_0.x;
+    ptDiff.y = d_0.y - s_0.y;
+    ptDiff.z = d_0.z - s_0.z;
+
 	float dist = ptDiff.x * ptDiff.x + ptDiff.y * ptDiff.y + ptDiff.z * ptDiff.z;
 
-	if (dist > distThresh) return false;
+    if (dist > distThresh)
+        return false;
 
-	corr3Dnormal = interpolateBilinear_withHoles(normalsMap, tmp2Dpoint, sceneImageSize);
-//	if (corr3Dnormal.w < 0.0f) return false;
-
-	b = corr3Dnormal.x * ptDiff.x + corr3Dnormal.y * ptDiff.y + corr3Dnormal.z * ptDiff.z;
+    b = n_0.x * ptDiff.x + n_0.y * ptDiff.y + n_0.z * ptDiff.z;
 
 	// TODO check whether normal matches normal from image, done in the original paper, but does not seem to be required
 	if (shortIteration)
 	{
 		if (rotationOnly)
 		{
-			A[0] = +tmp3Dpoint.z * corr3Dnormal.y - tmp3Dpoint.y * corr3Dnormal.z;
-			A[1] = -tmp3Dpoint.z * corr3Dnormal.x + tmp3Dpoint.x * corr3Dnormal.z;
-			A[2] = +tmp3Dpoint.y * corr3Dnormal.x - tmp3Dpoint.x * corr3Dnormal.y;
+            A[0] = +s_0.z * n_0.y - s_0.y * n_0.z;
+            A[1] = -s_0.z * n_0.x + s_0.x * n_0.z;
+            A[2] = +s_0.y * n_0.x - s_0.x * n_0.y;
 		}
-		else { A[0] = corr3Dnormal.x; A[1] = corr3Dnormal.y; A[2] = corr3Dnormal.z; }
+        else { A[0] = n_0.x; A[1] = n_0.y; A[2] = n_0.z; }
 	}
 	else
 	{
-		A[0] = +tmp3Dpoint.z * corr3Dnormal.y - tmp3Dpoint.y * corr3Dnormal.z;
-		A[1] = -tmp3Dpoint.z * corr3Dnormal.x + tmp3Dpoint.x * corr3Dnormal.z;
-		A[2] = +tmp3Dpoint.y * corr3Dnormal.x - tmp3Dpoint.x * corr3Dnormal.y;
-		A[!shortIteration ? 3 : 0] = corr3Dnormal.x; A[!shortIteration ? 4 : 1] = corr3Dnormal.y; A[!shortIteration ? 5 : 2] = corr3Dnormal.z;
+        A[0] = +s_0.z * n_0.y - s_0.y * n_0.z;
+        A[1] = -s_0.z * n_0.x + s_0.x * n_0.z;
+        A[2] = +s_0.y * n_0.x - s_0.x * n_0.y;
+        A[!shortIteration ? 3 : 0] = n_0.x; A[!shortIteration ? 4 : 1] = n_0.y; A[!shortIteration ? 5 : 2] = n_0.z;
 	}
 
 	return true;
