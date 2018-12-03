c906535609f00b6a071441c253673a0b3f927626
arennuit <are@lescompanions.com>
2018-12-03 12:58:33 +0100

    [ViveEngine] BugFix: had CUDA code compile after the conversion of Frame to a template.

--- InfiniTAM/ITMLib/Engine/DeviceSpecific/CUDA/ITMViewBuilder_CUDA.h
+++ InfiniTAM/ITMLib/Engine/DeviceSpecific/CUDA/ITMViewBuilder_CUDA.h
@@ -21,7 +21,7 @@ namespace ITMLib
 			void UpdateView(ITMView **view, ITMUChar4Image *rgbImage, ITMShortImage *rawDepthImage, bool useBilateralFilter, bool modelSensorNoise = false);
 			void UpdateView(ITMView **view, ITMUChar4Image *rgbImage, ITMFloatImage *depthImage);
 			void UpdateView(ITMView **view, ITMUChar4Image *rgbImage, ITMShortImage *depthImage, bool useBilateralFilter, ITMIMUMeasurement *imuMeasurement);
-            void UpdateView(ITMView **view, ITMUChar4Image *rgbImage, ITMShortImage *depthImage, bool useBilateralFilter, Eigen::Frame* mocapMeasurement);
+            void UpdateView(ITMView **view, ITMUChar4Image *rgbImage, ITMShortImage *depthImage, bool useBilateralFilter, Eigen::Framef* mocapMeasurement);
 
 			ITMViewBuilder_CUDA(const ITMRGBDCalib *calib);
 			~ITMViewBuilder_CUDA(void);
