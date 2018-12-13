4d52fc6da2a17745f68a436e25a222d485663a36
arennuit <are@lescompanions.com>
2018-12-14 00:12:14 +0100

    [MocapFusion] The ICL-NUIM format conversion used to be done in ITMDepthTracker::PreTrack(), it is now done in MocapFileEngine.

--- InfiniTAM/Engine/MocapFileEngine.cpp
+++ InfiniTAM/Engine/MocapFileEngine.cpp
@@ -30,7 +30,15 @@ bool MocapFileEngine::hasMoreMeasurements()
           qx, qy, qz, qw;
     m_fileStream >> idx >> x >> y >> z >> qx >> qy >> qz >> qw;
 
-    m_cachedFrame.setWXYZ(qw, qx, qy, qz, x, y, z);
+    Eigen::Framef f_tracker_base( qw, qx, qy, qz, x, y, z );
+    Eigen::Vector3f r_tracker_base = f_tracker_base.m_quat.toRotationVector();
+    Eigen::Vector3f p_tracker_base = f_tracker_base.m_pos;
+
+    Eigen::Vector3f r_tracker_base_corrected(-r_tracker_base.x(),  r_tracker_base.y(), -r_tracker_base.z());
+    Eigen::Vector3f p_tracker_base_corrected( p_tracker_base.x(), -p_tracker_base.y(),  p_tracker_base.z());
+
+    m_cachedFrame.m_quat.fromRotationVector(r_tracker_base_corrected);
+    m_cachedFrame.m_pos = p_tracker_base_corrected;
 
     return true;
 }
